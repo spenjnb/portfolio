2f12304cf10e3861c6b0d421173391c8378aefe7
Spencer <spenjnb@gmail.com>
2025-04-18 00:05:07 -0600

    Initial commit: Portfolio setup with Vite and React

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <main>
+      <h1>Welcome to My Portfolio</h1>
+      <p>This site will showcase my projects and skills.</p>
+    </main>
+  );
+};
---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(<App />);
+}
