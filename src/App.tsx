5cff5821a9934e75399c3e56a245032abcbf644e
Spencer <spenjnb@gmail.com>
2025-04-21 22:37:01 -0600

    feat: integrate React Router and Tailwind CSS, update project structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+
 export default function App() {
   return (
-    <main>
-      <h1>Welcome to My Portfolio</h1>
-      <p>This site will showcase my projects and skills.</p>
-    </main>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </Router>
   );
 };
